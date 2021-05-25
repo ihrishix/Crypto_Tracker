394eba7c13ae803bbcd8702411d3a295ce03dd8c
Rushikesh Suryawanshi <suryawanshirushikesh90@gmail.com>
2021-05-26 03:02:57 +0530

    Alert Dialog Implemented. Working. Final Touches remaining

--- app/src/main/java/com/hrishi/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/hrishi/cryptotracker/MainActivity.kt
@@ -2,13 +2,21 @@ package com.hrishi.cryptotracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.hrishi.cryptotracker.databinding.ActivityMainBinding
 
+const val TAG = "Main Actiivty"
 class MainActivity : AppCompatActivity() {
+
+    var asset_list = mutableListOf<asset>()
+    val Main_adapter = Main_RecyclerView_Adapter(asset_list)
 
     private lateinit var binding : ActivityMainBinding
 
@@ -18,24 +26,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        var temp_list = mutableListOf<asset>(
-            asset("Bitcoin", "BTC", "2344543"),
-            asset("Dogecoin", "DOGE", "290.433"),
-            asset("WRXcoin", "WRX", "54.32"),
-            asset("coinBase", "CBS", "0.0024"),
-            asset("Mera COin", "MCO", "234.343"),
-            asset("Bitcoin", "BTC", "2344543"),
-        asset("Dogecoin", "DOGE", "290.433"),
-        asset("WRXcoin", "WRX", "54.32"),
-        asset("coinBase", "CBS", "0.0024"),
-        asset("Mera COin", "MCO", "234.343")
-        )
 
-        val Main_adapter = Main_RecyclerView_Adapter(temp_list)
+
+
+
+
         binding.mainRecyclerView.adapter = Main_adapter
         binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
 
 
+        binding.addButton.setOnClickListener {
+            add_asset_dialog()
+
+        }
 
 
 
@@ -43,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun check_price(cryptoName : String = "BTC"){
+    private fun check_price(cryptoName : String = "BTC") : String{
 
         val volley_queue = Volley.newRequestQueue(this)     //Api Setup
 
@@ -53,7 +56,8 @@ class MainActivity : AppCompatActivity() {
         val jsonObjectRequest = object : JsonObjectRequest(
             Request.Method.GET, url, null,
             { response ->
-                //binding.textView2.text = response.getString("rate")
+                val rate : String = response.getString("rate").toString()
+                return rate
             },
             { error ->
                 //binding.textView2.text = error.message
@@ -73,4 +77,51 @@ class MainActivity : AppCompatActivity() {
         volley_queue.add(jsonObjectRequest)
 
     }
+
+
+    private fun add_asset_dialog(){
+
+        val view = LayoutInflater.from(this).inflate(R.layout.add_assset_dialog, null)
+
+        val add_dialog = android.app.AlertDialog.Builder(this)
+            .setTitle("Add Coin")
+            .setView(view)
+            .setPositiveButton("Add") { _ , _ ->
+                val me = view.findViewById<EditText>(R.id.addAsset_id)
+                Log.e(TAG, "add_asset_dialog: ${me.text}", )
+
+            }.setNegativeButton("Cancel"){ _, _ ->
+            }
+            .setCancelable(true).create()
+
+
+        add_dialog.show()
+
+
+
+    }
+
+    private fun add_asset(){
+
+
+
+        val addasset_id = findViewById<TextView>(R.id.addAsset_id)
+        val addasset_Quantity = findViewById<TextView>(R.id.addAsset_Quantity)
+        val addasset_pricebought = findViewById<TextView>(R.id.addAsset_pricebought)
+
+        val rate = "0"
+
+
+        asset_list.add(
+            asset(
+            addasset_id.text.toString(),
+                rate, addasset_Quantity.toString(), addasset_pricebought.toString()
+            )
+        )
+
+
+        Main_adapter.notifyDataSetChanged()
+
+    }
+
 }
