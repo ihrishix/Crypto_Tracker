9fdb9f46d292e25da2fee7db6447279c00325091
Rushikesh Suryawanshi <suryawanshirushikesh90@gmail.com>
2022-04-30 01:34:22 +0530

    Update Server_key.kt

--- app/src/main/java/com/hrishi/cryptotracker/Server_key.kt
+++ app/src/main/java/com/hrishi/cryptotracker/Server_key.kt
@@ -4,7 +4,7 @@ class Server_key {
 
     companion object{
         const val coin_api_header = "X-CoinAPI-Key"
-        const val coin_api_key = "32685D30-226E-4EE3-8AAE-CF6FC3E96124"
+        const val coin_api_key = ""
 
 
 
